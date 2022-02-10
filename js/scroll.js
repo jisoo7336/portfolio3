e0e5d880443f0aa47f13e45fafd5c3367ea6556a
jisoo7336 <93235120+jisoo7336@users.noreply.github.com>
2022-02-10 16:16:59 +0900

    2022.02.10

--- js/scroll.js
+++ js/scroll.js
@@ -1,41 +1,45 @@
 
-
-const container = document.querySelector('#container');
-let offset = 0;
-
-window.addEventListener('wheel', e => {
-  offset += Math.sign(e.deltaY) * 60;
-
-  if (offset < 0) {
-    offset = 0;
-  } else if (offset > 10000 - window.innerWidth) {
-    offset = 10000 - window.innerWidth;
-  }
-
-  container.style.transform = `translateX(-${offset}px`;
-
-});
-
 $(function(){
-  $('.s1').css("transform","translateX(0px)").css("opacity","1");
 
-  $('.s2').css("transform","translateX(0px)").css("opacity","1");
+  $('.textbox').css("transform","translateX(0px)").css("opacity","1");
+  
+  const container = document.querySelector('#container');
+  let offset = 0;
 
-  $('.s3').css("transform","translateX(0px)").css("opacity","1");
+  window.addEventListener('wheel', e => {
+    offset += Math.sign(e.deltaY) * 60;
 
-  $('.s4').css("transform","translateX(0px)").css("opacity","1");
+    if (offset < 0) {
+      offset = 0;
+    } else if (offset > 10000 - window.innerWidth) {
+      offset = 10000 - window.innerWidth;
+    }
 
-  $('.s5').css("transform","translateX(0px)").css("opacity","1");
+    container.style.transform = `translateX(-${offset}px`;
 
+    
 
-  // $(window).scroll(function(){
-  //     var ws=$(this).scrollTop();
+    if(offset>500){
+      $('.s1').css("transform","translateX(0px)").css("opacity","1");
+    }
 
-  //   var s4=$('.s4').offset().left;
-  //     if(ws>s4-500){
-  //       $('.s4').css("transform","translateY(0px)").css("opacity","1");
-  //     }
+    if(offset>1500){
+      $('.s2').css("transform","translateX(0px)").css("opacity","1");
+    }
 
-  // });
+    if(offset>3000){
+      $('.s3').css("transform","translateX(0px)").css("opacity","1");
+    }
+
+    if(offset>4500){
+      $('.s4').css("transform","translateX(0px)").css("opacity","1");
+    }
+
+    if(offset>5800){
+      $('.s5').css("transform","translateX(0px)").css("opacity","1");
+    }
+
+   
+  });
 
 });
