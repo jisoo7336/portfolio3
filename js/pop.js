50cc35deacf1500c1bbca4fbe2b3012532a3ccf4
jisoo7336 <93235120+jisoo7336@users.noreply.github.com>
2022-02-10 18:05:15 +0900

    2022.02.10

---
+++ js/pop.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+
+  $(".img1").click(function () {
+
+    $(".coverbox1").fadeIn();
+
+  });
+
+  $(".coverbox1>button").click(function () {
+
+    $(".img1").fadeOut();
+
+  });
+
+  $(".img2").click(function () {
+
+    $(".coverbox2").fadeIn();
+
+  });
+
+  $(".coverbox2>button").click(function () {
+
+    $(".img2").fadeOut();
+
+  });
+
+  $(".img3").click(function () {
+
+    $(".coverbox3").fadeIn();
+
+  });
+
+  $(".coverbox3>button").click(function () {
+
+    $(".img3").fadeOut();
+
+  });
+
+  $(".img4").click(function () {
+
+    $(".coverbox4").fadeIn();
+
+  });
+
+  $(".coverbox4>button").click(function () {
+
+    $(".img4").fadeOut();
+
+  });
+
+  $(".img5").click(function () {
+
+    $(".coverbox5").fadeIn();
+
+  });
+
+  $(".coverbox5>button").click(function () {
+
+    $(".img5").fadeOut();
+
+  });
+
+});
